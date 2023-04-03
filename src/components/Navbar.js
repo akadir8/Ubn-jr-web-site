1b64409cc7c210675a2441a54f2bbfcd3480056e
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-04 02:38:52 +0300

    onClick modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import logo from './logo2.png'
+import React, { useState } from 'react';
+import logo from './logo2.png';
 
 export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState(0);
+
   let Links = [
     {name:"Anasayfa",link:"/"},
     {name:"Web",link:"/"},
     {name:"Siber",link:"/"},
     {name:"Tasarım",link:"/"},
     {name:"Elektronik",link:"/"},
-  ]
+  ];
+
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex items-center justify-center bg-white py-4 md:px-10 px-7'>
@@ -20,19 +23,19 @@ export const Navbar = () => {
         </div>
         <ul className='md:flex md:items-center'>
         {
-          Links.map((link) => (
+          Links.map((link, index) => (
             <li key={link.name} className='md:ml-8 text-xl'>
-              <button onClick={() => window.location.href = link.link} className='text-gray-800 hover:text-gray-400 duration-500'>
-              {link.name}
+              <button 
+                onClick={() => setActiveLink(index)} 
+                className={`text-gray-800 hover:text-gray-400 duration-500 ${activeLink === index ? "font-bold" : ""}`}>
+                {link.name}
               </button>
             </li>
           ))
         }
-
         </ul>
       </div>
     </div>
   )
 }
 export default Navbar;
-
