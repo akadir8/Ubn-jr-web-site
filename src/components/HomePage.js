866fd4289ec2ed770581a76341a7714b76c182fa
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-05 02:06:24 +0300

    HomePage modified

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,10 +15,15 @@ function HomePage() {
       <div className="hidden lg:flex flex-col gap-5 rounded-md shadow-lg shadow-[#FC2947] absolute top-0 bottom-0 m-auto right-10 bg-white p-6 h-fit w-1/3">
         <h1 className="text-4xl font-bold text-cyan-700">Lorem, ipsum dolor.</h1>
         <p className="text-gray-800">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit ratione totam illum harum rerum magnam hic sed architecto dolorum deleniti vel, adipisci neque libero, ea eius repellendus. Ipsa, inventore perferendis!</p>
-        <div className="justify-between space-x-2 text-2xl text-[#002B5B]">
-          <FaYoutube />
-          <FaGithub />
+        <div className="space-x-3 text-2xl text-[#002B5B] flex">
+        <div>
+            <FaYoutube />
         </div>
+        <div>
+            <FaGithub />
+        </div>
+        </div>
+
       </div>
     </>
   );
