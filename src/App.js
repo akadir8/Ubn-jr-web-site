22bd456beea520aecd1e063b2c1472386ef53962
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-06 03:16:49 +0300

    app.js and navbar.js modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import logo from './components/logo2.png';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
-
+import Navbar from './components/Navbar';
 
 function App() {
   const [activeLink, setActiveLink] = useState(0);
-  let Links = [
+  const Links = [
     {name:"Anasayfa",link:"/"},
     {name:"Web",link:"/"},
     {name:"Siber",link:"/"},
@@ -14,43 +13,11 @@ function App() {
     {name:"Elektronik",link:"/"},
   ];
   return (
-    
-  
     <div className="App ">
-      <>
-      <div className='bg-[#E8D5C4] w-full h-screen'>
-      <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex items-center justify-center bg-white py-4 md:px-10 px-7'>
-        <div className='font-bold text-2xl cursor-pointer flex items-center font-font text-gray-800'>
-          <span>
-            <img src={logo} width={38} height={38} alt="" />
-          </span>
-          Ubn-Jr
-        </div>
-        <ul className='md:flex md:items-center'>
-        {
-          Links.map((link, index) => (
-            <li key={link.name} className='md:ml-8 text-xl'>
-              <button 
-                onClick={() => setActiveLink(index)} 
-                className={`text-gray-800 hover:text-gray-400 duration-500 ${activeLink === index ? "font-bold" : ""}`}>
-                {link.name}
-              </button>
-            </li>
-          ))
-        }
-        </ul>
-      </div>
-    </div>
-      </div>
+      <Navbar activeLink={activeLink} setActiveLink={setActiveLink} Links={Links} />
       <HomePage/>
       <Footer/>
-     
-      </>
-       
-      
     </div>
-    
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,35 @@
+import React from 'react';
+import logo from './logo2.png';
 
+function Navbar(props) {
+  const { activeLink, setActiveLink, Links } = props;
+
+  return (
+    <div className='bg-[#E8D5C4] w-full h-screen'>
+      <div className='shadow-md w-full fixed top-0 left-0'>
+        <div className='md:flex items-center justify-center bg-white py-4 md:px-10 px-7'>
+          <div className='font-bold text-2xl cursor-pointer flex items-center font-font text-gray-800'>
+            <span>
+              <img src={logo} width={38} height={38} alt="" />
+            </span>
+            Ubn-Jr
+          </div>
+          <ul className='md:flex md:items-center'>
+            {Links.map((link, index) => (
+              <li key={link.name} className='md:ml-8 text-xl'>
+                <button 
+                  onClick={() => setActiveLink(index)} 
+                  className={`text-gray-800 hover:text-gray-400 duration-500 ${activeLink === index ? "font-bold" : ""}`}
+                >
+                  {link.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
