a7d6040ad50bc147e0fb653c247df1f29c5b6f3f
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-08 02:24:19 +0300

    homepage.js, page.js, data.json modified

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -23,7 +23,7 @@ function HomePage() {
       </div>
       <div className="hidden lg:flex flex-col gap-5 rounded-md shadow-lg shadow-[#FC2947] absolute top-0 bottom-0 m-auto right-10 bg-white p-6 h-fit w-1/3">
         <h1 className="text-4xl font-bold text-cyan-700">Lorem, ipsum dolor.</h1>
-        <p className="text-gray-800">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit ratione totam illum harum rerum magnam hic sed architecto dolorum deleniti vel, adipisci neque libero, ea eius repellendus. Ipsa, inventore perferendis!</p>
+        <p className="text-gray-800 font-semibold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit ratione totam illum harum rerum magnam hic sed architecto dolorum deleniti vel, adipisci neque libero, ea eius repellendus. Ipsa, inventore perferendis!</p>
         <div className="space-x-3 text-2xl text-[#002B5B] flex">
         <div>
           <a href="https://www.youtube.com/@UbnJr">
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import HomePage from './HomePage';
+import jsonData from "./data.json";
 
 function Page({ activeLink }) {
   if (activeLink === 0) {
     return <HomePage />;
-  } else {
-    return <div>Başka bir sayfa</div>;
+  } else{
+    const link = [
+      {name:"Web", link:"/"},
+      {name:"Siber", link:"/"},
+      {name:"Tasarım", link:"/"},
+      {name:"Elektronik", link:"/"},
+    ][activeLink - 1];
+
+    const { header, content } = jsonData[link.name.toLowerCase()];
+
+    return (
+      <div className='hidden lg:flex flex-col gap-5 rounded-md shadow-lg shadow-[#FC2947] absolute top-56 m-auto left-10 bg-white p-6 h-fit w-1/3'>
+        <h2 className='text-4xl font-bold text-cyan-700'>{header}</h2>
+        <p className='text-gray-800 font-semibold'>{content}</p>
+      </div>
+    );
   }
 }
 
