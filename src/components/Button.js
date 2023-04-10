20cb8c03a08d66441da34f3d754250dd393e266d
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-11 02:12:12 +0300

    Button.js, Footer.js and Navbar.js modified

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button(props) {
   return (
-    <button className="mr-12 rounded-full py-2 px-3 text-xs font-bold cursor-pointer tracking-wider bg-cyan-600 text-white hover:bg-cyan-700 duration-100">
+    <button className="ml-56 rounded-full py-2 px-3 text-xs font-bold cursor-pointer tracking-wider bg-cyan-600 text-white hover:bg-cyan-700 duration-100">
       Bize katılmak ister misiniz ?
     </button>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,14 +13,14 @@ function Navbar(props) {
     <div className="bg-[#E8D5C4] w-full h-screen">
       <div className="shadow-md w-full fixed top-0 left-0">
         <div className="md:flex items-center justify-center bg-white py-4 md:px-10 px-7">
-          <div className="font-bold text-2xl cursor-pointer flex items-center font-font text-gray-800">
+          <div className="font-bold mr-48 text-2xl cursor-pointer flex items-center font-font text-gray-800">
             <span>
               <img src={logo} width={38} height={38} alt="" />
             </span>
             Ubn-Jr
           </div>
           {/* Navbar bileşeni, logo ve bağlantıları gösteren bir üstbilgi kısmı içerir. Logo, "Ubn-Jr" metniyle birlikte gösterilir ve logo2.png dosyasından alınır.  */}
-          <ul className="md:flex md:items-center">
+          <ul className=" md:flex md:items-center">
             {Links.map((link, index) => (
               <li key={link.name} className="md:ml-8 text-xl">
                 <button
