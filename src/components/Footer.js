c6d9023bdf80e81fd4b485fcba8d68c472b8ce35
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-10 12:35:36 +0300

    Footer.js description added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
+/* Footer bileşenini tanımladık. Footer, bir <div> etiketi içinde bir <p> etiketi ile oluşturduk. Bu paragraf etiketi, "Ubn-Jr. Tüm Hakları Saklıdır" metnini ve 2023 yılını gösteren telif hakkı uyarısını ekranımıza yazdırdık. */
 function Footer(){
     return (
+        /* className özelliği, CSS sınıflarını tanımlamak için kullanılır. Bu örnekte, <div> etiketi <p> etiketi içerisine css özellikerimizi tanımladık. */
         <div className="bg-[#3A98B9] font-nunito dark">
             <p className="text-sm font-bold text-center">Copyright © 2023 Ubn-Jr. All Right Reserved</p>
 
         </div>
 )
 }
-export default Footer
+export default Footer;
+
+/* Footer bileşeni, "export default" anahtar sözcüğü ile dışa aktarılır, böylece bu bileşen diğer dosyalardan kullanılabilir hale gelir. */
