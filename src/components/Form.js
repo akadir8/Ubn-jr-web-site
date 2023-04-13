16ecd317383a922bceafcc589eb23d14eaae70e0
akadir8 <bickiabdulkadir12@gmail.com>
2023-04-14 00:16:39 +0300

    Form.js and Toast.js modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -27,89 +27,91 @@ const Form = ({ onClose }) => {
 
   return (
     <div className="fixed z-50 inset-0 overflow-y-auto">
-    {showToast && (
-      <Toast
-        message="Bilgileriniz başarıyla gönderildi!"
-        onClose={handleToastClose}
-      />
-    )}
-    <div className="fixed z-50 inset-0 overflow-y-auto">
-      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 transition-opacity">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
-        &#8203;
-        <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-          <button
-            type="button"
-            className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-            onClick={onClose}
-          >
-            X
-          </button>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-6">
-              <h1 className="text-xl font-bold mb-2">İSİM :</h1>
-
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="border border-gray-400 p-2 w-full rounded-md"
-                placeholder="İsminizi Giriniz..."
-              />
-            </div>
-            <div className="mb-6">
-              <h1 className="text-xl font-bold mb-2">SOY İSİM :</h1>
-
-              <input
-                type="text"
-                id="surname"
-                name="surname"
-                value={formData.surname}
-                onChange={handleChange}
-                className="border border-gray-400 p-2 w-full rounded-md"
-                placeholder="Soyisminizi Giriniz..."
-              />
-            </div>
-            <div className="mb-6">
-              <h1 className="text-xl font-bold mb-2">E MAİL :</h1>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="border border-gray-400 p-2 w-full rounded-md"
-                placeholder="E-mail'inizi Giriniz..."
-              />
-            </div>
-            <div className="mb-6">
-              <h1 className="text-xl font-bold mb-2">MESAJ :</h1>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                className="border border-gray-400 p-2 w-full rounded-md"
-                placeholder="Mesajınızı Giriniz..."
-              />
-            </div>
+      {showToast && (
+        <Toast
+          message="Bilgileriniz başarıyla gönderildi!"
+          onClose={handleToastClose}
+        />
+      )}
+      <div className="fixed z-50 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+          &#8203;
+          <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
             <div className="text-right">
-              <button
-                type="submit"
-                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                GÖNDER
-              </button>
+            <button
+              type="button"
+              className="inline-flex justify-center py-1 px-2 border border-transparent shadow-sm text-xs font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              onClick={onClose}
+            >
+              X
+            </button>
             </div>
-          </form>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">İSİM :</h1>
+
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="border border-gray-400 p-2 w-full rounded-md"
+                  placeholder="İsminizi Giriniz..."
+                />
+              </div>
+              <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">SOY İSİM :</h1>
+
+                <input
+                  type="text"
+                  id="surname"
+                  name="surname"
+                  value={formData.surname}
+                  onChange={handleChange}
+                  className="border border-gray-400 p-2 w-full rounded-md"
+                  placeholder="Soyisminizi Giriniz..."
+                />
+              </div>
+              <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">E MAİL :</h1>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="border border-gray-400 p-2 w-full rounded-md"
+                  placeholder="E-mail'inizi Giriniz..."
+                />
+              </div>
+              <div className="mb-6">
+                <h1 className="text-xl font-bold mb-2">MESAJ :</h1>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="border border-gray-400 p-2 w-full rounded-md"
+                  placeholder="Mesajınızı Giriniz..."
+                />
+              </div>
+              <div className="text-right">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  GÖNDER
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/components/Toast.js
+++ src/components/Toast.js
@@ -20,7 +20,7 @@ const Toast = ({ message, onClose }) => {
   /* Render metodu, bir <div> öğesi içinde belirtilen mesajı gösterir. "show" değişkeni kullanılarak, belirli bir animasyonlu geçişle bileşenin görünür veya gizlenir hale getirilmesi sağlanır. */
   return (
     <div
-      className={`fixed top-0 right-0 m-8 bg-gray-800 text-white px-4 py-2 rounded-md transition-all duration-500 ${
+      className={`fixed bottom-0 right-0 m-8 bg-gray-800 text-white px-4 py-2 rounded-md transition-all duration-500 ${
         show ? "opacity-100 visible" : "opacity-0 invisible"
       }`}
     >
