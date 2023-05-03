b037174a61046aaf5bb85adcda85f8d34d5b0cfb
akadir8 <bickiabdulkadir12@gmail.com>
2023-05-03 13:59:46 +0300

    SocialMedia modified

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,12 +5,12 @@ function SocialMedia() {
     <>
       <div className="space-x-3 text-2xl text-[#002B5B] flex">
         <div>
-        <a href="https://www.youtube.com/@UbnJr" target="_blank">
+        <a href="https://www.youtube.com/@UbnJr" target="_blank" rel="noopener noreferrer">
             <FaYoutube />
           </a>
         </div>
         <div>
-          <a href="https://github.com/orgs/Ubn-Jr/dashboard" target="_blank">
+          <a href="https://github.com/orgs/Ubn-Jr/dashboard" target="_blank" rel="noopener noreferrer">
             <FaGithub />
           </a>
         </div>
