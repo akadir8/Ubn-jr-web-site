ffe4cd2012c02db15a8ed77c80945ab8d2b0b4a9
akadir8 <bickiabdulkadir12@gmail.com>
2023-05-30 19:35:32 +0300

    Updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -33,13 +33,20 @@ function Card(props) {
   return (
     <>
       <div className="card bg-white border border-gray-300 rounded-lg" style={cardStyle}>
-        <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
-          {data.title}
-        </h2>
-        <div className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`} style={{ overflow: "hidden" }}>
-          <p className="text-left" style={{ marginTop: "-10px" }}>
-            {data.content}
-          </p>
+        {data && data.title && (
+          <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
+            {data.title}
+          </h2>
+        )}
+        <div
+          className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`}
+          style={{ overflow: "hidden" }}
+        >
+          {data && data.content && (
+            <p className="text-left" style={{ marginTop: "-10px" }}>
+              {data.content}
+            </p>
+          )}
         </div>
         <button
           type="button"
@@ -53,10 +60,12 @@ function Card(props) {
 
       <Modal isOpen={showModal} onRequestClose={toggleModal}>
         <div className="modal-content">
-          <h2 className="text-center text-2xl font-bold" style={{ marginTop: "15px", marginBottom: "10px" }}>
-            {data.title}
-          </h2>
-          <p>{data.content}</p>
+          {data && data.title && (
+            <h2 className="text-center text-2xl font-bold" style={{ marginTop: "15px", marginBottom: "10px" }}>
+              {data.title}
+            </h2>
+          )}
+          {data && data.content && <p>{data.content}</p>}
           <button
             type="button"
             className=" bg-cyan-600 hover:bg-[#569DAA] text-white font-bold py-1 px-2 rounded"
