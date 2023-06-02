e06fe3a8435d019ced3f8ef955d3b59fe71994e9
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-02 16:30:28 +0300

    Card.js Updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 function Card(props) {
   const [showModal, setShowModal] = useState(false);
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -32,40 +32,46 @@ function Card(props) {
 
   return (
     <>
-      <div className="card bg-white border border-gray-300 rounded-lg" style={cardStyle}>
-        {data && data.title && (
-          <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
-            {data.title}
-          </h2>
-        )}
-        <div
-          className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`}
-          style={{ overflow: "hidden" }}
-        >
-          {data && data.content && (
-            <p className="text-left" style={{ marginTop: "-10px" }}>
-              {data.content}
-            </p>
+      {data.map((item, index) => (
+        <div key={index} className="card bg-white border border-gray-300 rounded-lg" style={cardStyle}>
+          {item.title && (
+            <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
+              {item.title}
+            </h2>
           )}
+          <div
+            className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`}
+            style={{ overflow: "hidden" }}
+          >
+            {item.content && (
+              <p className="text-left" style={{ marginTop: "-10px" }}>
+                {item.content}
+              </p>
+            )}
+          </div>
+          <button
+            type="button"
+            className="bg-cyan-600 hover:bg-[#569DAA] text-white font-bold py-1 px-2 rounded"
+            onClick={toggleModal}
+            style={{ position: "absolute", bottom: "10px", right: "10px" }}
+          >
+            Devamı..
+          </button>
         </div>
-        <button
-          type="button"
-          className="bg-cyan-600 hover:bg-[#569DAA] text-white font-bold py-1 px-2 rounded"
-          onClick={toggleModal}
-          style={{ position: "absolute", bottom: "10px", right: "10px" }}
-        >
-          Devamı..
-        </button>
-      </div>
+      ))}
 
       <Modal isOpen={showModal} onRequestClose={toggleModal}>
         <div className="modal-content">
-          {data && data.title && (
-            <h2 className="text-center text-2xl font-bold" style={{ marginTop: "15px", marginBottom: "10px" }}>
-              {data.title}
-            </h2>
-          )}
-          {data && data.content && <p>{data.content}</p>}
+          {data.map((item, index) => (
+            <div key={index}>
+              {item.title && (
+                <h2 className="text-center text-2xl font-bold" style={{ marginTop: "15px", marginBottom: "10px" }}>
+                  {item.title}
+                </h2>
+              )}
+              {item.content && <p>{item.content}</p>}
+            </div>
+          ))}
           <button
             type="button"
             className=" bg-cyan-600 hover:bg-[#569DAA] text-white font-bold py-1 px-2 rounded"
