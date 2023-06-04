f7d76d4666792bbb2e7efabf0dcbdbc6bbec404c
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-04 03:08:33 +0300

    Card.js Updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,28 +9,27 @@ function Card({ cardContent }) {
   }
 
   const cardStyle = {
-    position: "absolute",
-    ...cardContent.position,
     width: cardContent.width,
     height: cardContent.height,
   };
 
   return (
     <>
-      <div className="card bg-white border border-gray-300 rounded-lg" style={cardStyle}>
-        <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
-          {cardContent.title}
-        </h2>
-        <div className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`} style={{ overflow: "hidden" }}>
-          <p className="text-left" style={{ marginTop: "-10px" }}>
-            {cardContent.content}
-          </p>
+      <div className="card bg-white border border-gray-300 rounded-lg flex flex-col justify-between" style={cardStyle}>
+        <div>
+          <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
+            {cardContent.title}
+          </h2>
+          <div className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`} style={{ overflow: "hidden" }}>
+            <p className="text-left" style={{ marginTop: "-10px" }}>
+              {cardContent.content}
+            </p>
+          </div>
         </div>
         <button
           type="button"
           className="bg-cyan-600 hover:bg-[#569DAA] text-white font-bold py-1 px-2 rounded"
           onClick={toggleModal}
-          style={{ position: "absolute", bottom: "10px", right: "10px" }}
         >
           Devamı..
         </button>
@@ -41,9 +40,7 @@ function Card({ cardContent }) {
           <h2 className="text-center text-2xl font-bold" style={{ marginTop: "15px", marginBottom: "10px" }}>
             {cardContent.title}
           </h2>
-          <p>
-            {cardContent.content}
-          </p>
+          <p>{cardContent.content}</p>
           <p>Yazan: {cardContent.author}</p>
           <button
             type="button"
