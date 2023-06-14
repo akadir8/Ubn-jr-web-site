c8844aa2f7ae81c4af1df42ae3ffac29ba89b4eb
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-14 12:39:05 +0300

    Moved to separate file

---
+++ src/Buttons/ContactButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+function Button({ showForm }) {
+  
+  return (
+    <button id="buton"
+      onClick={showForm}
+      className="ml-56 rounded-full py-2 px-3 text-xs font-bold tracking-wider text-white hover:bg-cyan-700 duration-100"
+    >
+      Bizimle iletişime geçin
+    </button>
+  );
+}
+ 
+export default Button;
