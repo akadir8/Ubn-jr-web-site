6585972e05e2fe4c0899a770f407870b341cf591
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-14 12:39:33 +0300

    Moved to separate file

---
+++ src/Layouts/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="font-nunito" style={{ position: "fixed", bottom: 0, width: "100%" }}>
+      <p className="text-sm font-bold text-center">
+        Copyright © 2023 Ubn-Jr. All Right Reserved
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Layouts/Navbar.js
@@ -0,0 +1,77 @@
+import React from "react";
+import logo from "../Assets/ubnjr.png";
+import Button from "../Buttons/ContactButton";
+import Form from "../Forms/ContactForm";
+import Toast from "../components/Toast";
+
+function Navbar(props) {
+  const { activeLink, setActiveLink, Links } = props;
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [showToast, setShowToast] = React.useState(false);
+
+  const toggleForm = () => setIsOpen(!isOpen);
+
+  const toggleToast = () => {
+    setShowToast(true);
+
+    setTimeout(() => {
+      setShowToast(false);
+    }, 3000);
+  };
+
+  const handleClickLogo = () => {
+    window.location.href = "/";
+  };
+
+  return (
+    <div className="bg- w-full h-screen">
+      <div className="shadow-md w-full fixed top-0 left-0">
+        <div
+          id="navBar"
+          className="md:flex items-center justify-center py-4 md:px-10 px-7"
+        >
+          <div
+            className="font-bold mr-48 text-2xl cursor-pointer flex items-center font-font text-sky-800"
+            onClick={handleClickLogo}
+          >
+            <span>
+              <img
+                src={logo}
+                width={48}
+                height={48}
+                alt=""
+                style={{ marginRight: "10px" }}
+                title="Ubn-Jr"
+              />
+            </span>
+            <div>
+              <p>Ubn-Jr</p>
+              <p className="text-xs">You Be Not Junior!</p>
+            </div>
+          </div>
+          <ul className=" md:flex md:items-center">
+            {Links.map((link, index) => (
+              <li key={link.name} className="md:ml-8 text-xl">
+                <button
+                  onClick={() => setActiveLink(index)}
+                  className={`text-sky-800 hover:text-gray-400 duration-100 ${
+                    activeLink === index ? "font-bold" : ""
+                  }`}
+                >
+                  {link.name}
+                </button>
+              </li>
+            ))}
+            <li className="md:ml-8">
+              <Button showForm={toggleForm} />
+            </li>
+          </ul>
+        </div>
+      </div>
+      {isOpen && <Form toggleForm={toggleForm} toggleToast={toggleToast} />}
+      {showToast && <Toast message="Bilgileriniz başarıyla gönderildi." />}
+    </div>
+  );
+}
+
+export default Navbar;
