8c511e60924c953b8f288f7f389ef4ed240a9de4
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-14 15:18:41 +0300

    Cards.js updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,12 +15,20 @@ function Card({ cardContent }) {
 
   return (
     <>
-      <div className="card bg-white border border-gray-300 rounded-lg flex flex-col justify-between" style={cardStyle}>
+      <div
+        className="card bg-white border border-gray-300 rounded-lg flex flex-col justify-between"
+        style={cardStyle}
+      >
         <div>
           <h2 className="text-center font-bold" style={{ marginTop: "20px" }}>
             {cardContent.title}
           </h2>
-          <div className={`card-content ${showModal ? "card-content-show" : "card-content-opacity"}`} style={{ overflow: "hidden" }}>
+          <div
+            className={`card-content ${
+              showModal ? "card-content-show" : "card-content-opacity"
+            }`}
+            style={{ overflow: "hidden" }}
+          >
             <p className="text-left" style={{ marginTop: "-10px" }}>
               {cardContent.content}
             </p>
@@ -37,11 +45,19 @@ function Card({ cardContent }) {
 
       <Modal isOpen={showModal} onRequestClose={toggleModal}>
         <div className="modal-content">
-          <h2 className="text-center text-2xl font-bold" style={{ marginTop: "15px", marginBottom: "10px" }}>
+          <h2
+            className="text-center text-2xl font-bold"
+            style={{ marginTop: "15px", marginBottom: "10px" }}
+          >
             {cardContent.title}
           </h2>
           <p>{cardContent.content}</p>
-          <p>Yazan: {cardContent.author}</p>
+          <div
+            className="author-info"
+            style={{ position: "absolute", bottom: "-50px", right: "10px" }}
+          >
+            <p>Yazan: {cardContent.author}</p>
+          </div>
           <button
             type="button"
             className="bg-cyan-600 hover:bg-[#569DAA] text-white font-bold py-1 px-2 rounded"
