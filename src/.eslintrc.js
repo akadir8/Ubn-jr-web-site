bb38f0d7961c942b824b726f4874c9b11b3d9d6b
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-14 10:25:12 +0300

    .eslintrc.js added

---
+++ src/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {},
+};
