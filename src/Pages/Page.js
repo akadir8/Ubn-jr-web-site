257b707938772379d341c662c8d80462f65402af
akadir8 <bickiabdulkadir12@gmail.com>
2023-06-20 13:08:26 +0300

    Page.js Updated

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -10,9 +10,6 @@ import TextForm from "../Forms/TextForm";
 function Page({ activeLink }) {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleForm = () => setIsOpen(!isOpen);
-  useEffect(() => {
-    document.documentElement.style.overflow = "hidden";
-  }, []);
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -51,27 +48,30 @@ function Page({ activeLink }) {
             <SocialMedia />
           </p>
         </div>
-        <div className="flex flex-wrap justify-center gap-8">
-          {data.length > 0 &&
-            data
-              .map((item, index) => (
-                <div
-                  key={index}
-                  style={{ position: "relative", top: "-480px", left: "280px" }}
-                >
-                  <Card
-                    cardContent={{
-                      position: item.position,
-                      width: "250px",
-                      height: "300px",
-                      title: item.title,
-                      content: item.content,
-                      author: item.author,
-                    }}
-                  />
-                </div>
-              ))
-              .reverse()}
+        <div className="flex justify-center gap-8">
+          <div
+            className="grid grid-cols-3 gap-4"
+            style={{ gridAutoFlow: "row" }}
+          >
+            {data.length > 0 &&
+              data
+                .map((item, index) => (
+                  <div key={index}
+                  style={{ position: "relative", top: "-480px", left: "280px" }}>
+                    <Card
+                      cardContent={{
+                        position: item.position,
+                        width: "250px",
+                        height: "300px",
+                        title: item.title,
+                        content: item.content,
+                        author: item.author,
+                      }}
+                    />
+                  </div>
+                ))
+                .reverse()}
+          </div>
         </div>
 
         <div className="fixed bottom-5 right-5">
